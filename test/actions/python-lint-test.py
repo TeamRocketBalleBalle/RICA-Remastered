3a67d88303967db31880b722d2773faad2b33f9e
RoguedBear <39442192+RoguedBear@users.noreply.github.com>
2021-09-20 15:55:31 +0000

    Create python-lint-test.py

---
+++ test/actions/python-lint-test.py
@@ -0,0 +1,25 @@
+import os
+import sys
+
+from my_lib import Object, Object2, Object3
+from third_party import lib1, lib2, lib3, lib4, lib5, lib6, lib7, lib8, lib9, lib10, lib11, lib12, lib13, lib14, lib15
+
+x = max(1, 2, 3)
+
+
+def foo_decl(
+        a,
+    b,
+        c):
+    pass
+
+
+{
+    "green": 42,
+    "eggs and ham": -0.0e0
+}
+
+odds = [
+    num for num in range(42)
+    if num % 2 != 0
+]
