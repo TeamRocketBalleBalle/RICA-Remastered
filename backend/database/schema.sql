9dd3abae291a6ceda88336a8cf22fca5707f2615
Yash Aggarwal <vasu2013agg@gmail.com>
2021-10-14 16:12:25 +0530

    Changes in Db schema
    Changes suggested as in last meeting has been implemented...
    Changes:
    - New Table Appointment
    - Doctor table has new columns(Appointment, slot, Bio)
    - Name of few columns are changed

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -1,36 +1,48 @@
 CREATE DATABASE IF NOT EXISTS rica;
 
 CREATE TABLE IF NOT EXISTS user (
-    UiD varchar(10) NOT NULL PRIMARY KEY,
+    UserID varchar(10) NOT NULL PRIMARY KEY,
     Name varchar(30) NOT NULL,
     Email varchar(50) NOT NULL,
     Phone int NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS patient (
-    PiD varchar(10) NOT NULL,
+    PatientID varchar(10) NOT NULL,
     Name varchar(30) NOT NULL,
-    FOREIGN KEY (PiD) REFERENCES user(UiD)
+    FOREIGN KEY (PatientID) REFERENCES user(UserID)
 );
 
 CREATE TABLE IF NOT EXISTS doctor(
-    DiD varchar(10) NOT NULL,
+    DoctorID varchar(10) NOT NULL,
     Name varchar(30) NOT NULL,
     Degree varchar(15) NOT NULL,
-    FOREIGN KEY (DiD) REFERENCES user(UiD)
+    Bio JSON NOT NULL,
+    slot JSON NOT NULL,
+    appointment JSON NOT NULL,
+    FOREIGN KEY (DoctorID) REFERENCES user(UserID)
 );
 
 CREATE TABLE IF NOT EXISTS chemist(
-    CiD varchar(10) NOT NULL,
+    ChemistID varchar(10) NOT NULL,
     Name varchar(30) NOT NULL,
     location varchar(30) NOT NULL,
-    FOREIGN KEY (CiD) REFERENCES user(UiD)
+    FOREIGN KEY (ChemistID) REFERENCES user(UserID)
 );
 
 CREATE TABLE IF NOT EXISTS orders(
     PiD varchar(10) NOT NULL,
     CiD varchar(10) NOT NULL,
     prescription JSON NOT NULL,
-    FOREIGN KEY (PiD) REFERENCES patient(PiD),
-    FOREIGN KEY (CiD) REFERENCES chemist(CiD)
+    FOREIGN KEY (PiD) REFERENCES patient(PatientID),
+    FOREIGN KEY (CiD) REFERENCES chemist(ChemistID)
+);
+
+CREATE TABLE IF NOT EXISTS appointments(
+    BookingID JSON NOT NULL,
+    DiD varchar(10) NOT NULL,
+    PiD varchar(10) NOT NULL,
+    Timings TIMESTAMP NOT NULL,
+    FOREIGN KEY (PiD) REFERENCES patient(PatientID),
+    FOREIGN KEY (PiD) REFERENCES doctor(DoctorID)
 );
