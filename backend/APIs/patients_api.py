cd687ef3b912a82aae4b919deb809d562df8616d
Yash Aggarwal <vasu2013agg@gmail.com>
2021-11-06 15:58:38 +0530

    API making[Testing required]
    Most of the patient side APIs has been made
    Info required on few..
    
    @RoguedBear your help required for testing these APIs functions....

---
+++ backend/APIs/patients_api.py
@@ -0,0 +1,25 @@
+import flask
+from flask import current_app, jsonify, request
+
+from backend.APIs.doctors_api import get_booking_info
+from backend.utility.db_wrapper import get_cursor
+
+bp = flask.Blueprint("patients_api", __name__, url_prefix="/api/v1/patients")
+
+
+def previous_appointment(Patientid):
+    return get_booking_info(Patientid)
+
+
+@get_cursor
+def get_doc_names(city, specialization, cursor):
+    query = "SELECT Name FROM doctor where City = %s AND Specialization = %s"
+    cursor.execute(query, (city, specialization))
+    return jsonify(cursor.fetchall())
+
+
+@get_cursor
+def search_doctors_using_name(Name: str, cursor):
+    query = "SELECT Name from doctor where Name = %s"
+    cursor.execute(query, (Name,))
+    return jsonify(cursor.fetchall()[0])
