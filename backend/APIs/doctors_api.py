3e1011688a2f21f48df6f96a4780c6f9162b0ff0
Yash Aggarwal <vasu2013agg@gmail.com>
2021-11-13 17:40:40 +0530

    API updates
    > Add exceptional Handling in doctor_api.py , patient_api.py

--- backend/APIs/doctors_api.py
+++ backend/APIs/doctors_api.py
@@ -29,15 +29,17 @@ def get_booking_info(cursor):
 
     # if somehow we have non-existent user id in the cookie
     if userType is None:
-        reason = {"status": "BAD REQUEST",
-                  "reason": f"\"{user_id}\" is not a valid user_id"
-                  }
+        reason = {
+            "status": "BAD REQUEST",
+            "reason": f"\"{user_id}\" is not a valid user_id"
+        }
         return jsonify(reason), 400
     # for chemists, unauthorised for them since they dont have bookings
     elif userType[0] == "chemist":
-        reason = {"status": "FORBIDDEN",
-                  "reason": "Chemists do not have appointments"
-                  }
+        reason = {
+            "status": "FORBIDDEN",
+            "reason": "Chemists do not have appointments"
+        }
         return jsonify(reason), 403
 
     # TODO: limit appointments by result
@@ -45,7 +47,7 @@ def get_booking_info(cursor):
     response = {"appointments": []}
 
     query = "SELECT * FROM appointments WHERE %s IN (DoctorID, PatientID);"
-    cursor.execute(query, (user_id, ))
+    cursor.execute(query, (user_id,))
     for row in cursor:
         appointment = {
             "doctor_id": row[1],
@@ -56,4 +58,49 @@ def get_booking_info(cursor):
         # print(row[3].isoformat())
         response["appointments"].append(appointment)
 
+    return jsonify(response), 200
+
+
+@bp.route("/new_appointment/", methods=['GET'])
+@get_cursor
+def get_new_appointment_details(cursor):
+    doctor_id = session.get("id", "")
+    # not possible since user input is never involved. aka set by the server, but still checking for "oddities
+    if not isinstance(doctor_id, int):
+        reason = {
+            "status": "BAD REQUEST",
+            "reason": f"\"{doctor_id}\" is not a valid doctor_id"
+        }
+        return jsonify(reason), 400
+
+    query = " select userrole from users where UserID = %s"
+    cursor.execute(query, (doctor_id,))
+    userType = cursor.fetchone()
+
+    # if somehow we have non-existent user id in the cookie
+    if userType is None:
+        reason = {
+            "status": "BAD REQUEST",
+            "reason": f"\"{doctor_id}\" is not a valid doctor_id"
+        }
+        return jsonify(reason), 400
+
+    # for chemists, unauthorised for them since they dont have bookings
+    elif userType[0] == "chemist":
+        reason = {
+            "status": "FORBIDDEN",
+            "reason": "Chemists do not have appointments"
+        }
+        return jsonify(reason), 403
+
+    query = "SELECT a.BookingID, a.Timings, u.Name FROM appointments a, users u where Confirmed = false and DoctorID = %s and u.UserID = a.PatientID;"
+    cursor.execute(query, (doctor_id,))
+    response = {'booking_info': []}
+    for row in cursor:
+        booking_details = {
+            'booking_id': row[0],
+            'timings': row[1],
+            'patient_name': row[2]
+        }
+        response['booking_info'].append(booking_details)
     return jsonify(response), 200
--- backend/APIs/patients_api.py
+++ backend/APIs/patients_api.py
@@ -1,25 +1,77 @@
 import flask
-from flask import jsonify
+from flask import jsonify, session
 
-from backend.APIs.doctors_api import get_booking_info
 from backend.utility.db_wrapper import get_cursor
 
 bp = flask.Blueprint("patients_api", __name__, url_prefix="/api/v1/patients")
 
 
-def previous_appointment(Patientid):
-    return get_booking_info(Patientid)
-
-
+# TODO: Check if the below URL is working or not
+@bp.route("/get_doc_names/<string:city>/<string:specialization>", methods=['GET'])
 @get_cursor
 def get_doc_names(city, specialization, cursor):
-    query = "SELECT Name FROM doctor where City = %s AND Specialization = %s"
+    if not isinstance(city, str):
+        reason = {
+            "status": "BAD REQUEST",
+            "reason": "Invalid city"
+        }
+        return jsonify(reason), 400
+    if not isinstance(specialization, str):
+        reason = {
+            "status": "BAD REQUEST",
+            "reason": "Invalid specialization"
+        }
+        return jsonify(reason), 400
+
+    query = "SELECT u.Name, u.UserID, u.Phone FROM users u, doctor d where u.Location = %s AND d.Specialization = %s"
     cursor.execute(query, (city, specialization))
-    return jsonify(cursor.fetchall())
+    response = {'doctor_details': []}
+    for row in cursor:
+        doctor_detail = {
+            "doctor_name": row[0],
+            "doctor_id": row[1],
+            "phone_number": row[2]
+        }
+        response["doctor_details"].append(doctor_detail)
+    return jsonify(response), 200
 
 
+@bp.route("/search_doctor/<string:Name>", methods=['GET'])
 @get_cursor
-def search_doctors_using_name(Name: str, cursor):
-    query = "SELECT Name from doctor where Name = %s"
-    cursor.execute(query, (Name,))
-    return jsonify(cursor.fetchall()[0])
+def search_doctors_using_name(Name, cursor):
+    if not isinstance(Name, str):
+        reason = {
+            "status": "BAD REQUEST",
+            "reason": "Invalid name"
+        }
+        return jsonify(reason), 400
+    # query = " SELECT u.Name, d.DoctorID from users u, doctor d where u.Name like %%s% and u.userrole = 'doctor' and u.UserID = d.DoctorID;"
+    # cursor.execute(query, (Name,))
+
+    # Don't use 👆 commented syntax because we need: like '%<char>'
+    # But we are receiving: %'<char>'%
+    # I was not able to find a better way than the following method 👇
+    # We are using like because if the input is `S` it can still give output as (S4DGE, 3)
+    cursor.execute(
+        "SELECT u.Name, d.DoctorID, u.Phone from users u, doctor d where u.Name like '%%s%' and u.userrole = 'doctor' and u.UserID = d.DoctorID;",
+        Name)
+    response = {'doctor_details': []}
+
+    # We need doctor id because when add_new_appointment() function will be called then
+    # to store database we also need doctor id and I am hoping from Front End that they will
+    # give that function the doctor id
+    for row in cursor:
+        doctor_detail = {
+            "doctor_name": row[0],
+            "doctor_id": row[1],
+            "phone_Number": row[2]
+        }
+        response["doctor_details"].append(doctor_detail)
+    return jsonify(response), 200
+
+
+@bp.route('/new_appointment/', methods=['GET'])
+@get_cursor
+def add_new_appointment(doctor_id, cursor):
+    patient_id = session.get("id", "")
+    # TODO: Ponder the question how will it receive time and Symptoms? Will it be a JSON?
