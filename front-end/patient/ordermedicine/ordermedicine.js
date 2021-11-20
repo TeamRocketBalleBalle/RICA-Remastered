045a6885a8da2949c16614c310c9732959388cf5
aaaayuushhhh <77827902+aaaayuushhhh@users.noreply.github.com>
2021-11-20 16:22:58 +0530

    added files

---
+++ front-end/patient/ordermedicine/ordermedicine.js
@@ -0,0 +1,25 @@
+let form1 = document.getElementById("formlol");
+let json = {};
+form1.addEventListener("submit", (event) => {
+  event.preventDefault();
+  console.log(event);
+  let form1_data = event;
+  for (form_data_arr of new FormData(document.getElementById("formlol"))) {
+    json[form_data_arr[0]] = form_data_arr[1];
+    console.log(form_data_arr);
+  }
+  console.log(json);
+  console.log(JSON.stringify(json));
+});
+//code to convert data
+const toUrlEncoded = (obj) =>
+  Object.keys(obj)
+    .map((k) => encodeURIComponent(k) + "=" + encodeURIComponent(obj[k]))
+    .join("&");
+fetch("link where we post data", {
+  method: "POST",
+  body: toUrlEncoded(json),
+  headers: {
+    "Content-Type": "application/x-www-form-urlencoded",
+  },
+});
