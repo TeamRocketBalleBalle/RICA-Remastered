c53367e4d3b1e64dbf5257ee50b91cb781b50eb5
LAPTOP-BROQ7KSJ\shrey <shreyanshsharma480@gmail.com>
2021-11-21 22:34:22 +0530

    Removed Hey RandomUser69

--- front-end/patient/ordermedicine/ordermedicine.js
+++ front-end/patient/ordermedicine/ordermedicine.js
@@ -1,3 +1,13 @@
+$.ajax({
+  method: "GET",
+  url: "https://reqres.in/api/users",
+  success: function (response) {
+    myArray = response.data;
+    buildTable(myArray);
+    console.log(myArray);
+  },
+});
+
 let form1 = document.getElementById("formlol");
 let json = {};
 form1.addEventListener("submit", (event) => {
@@ -23,3 +33,12 @@ fetch("link where we post data", {
     "Content-Type": "application/x-www-form-urlencoded",
   },
 });
+
+function buildTable(data) {
+  var table = document.getElementById("lol");
+
+  for (var i = 0; i < data.length; i++) {
+    var name = `${data[i].id}`;
+    table.innerHTML += name;
+  }
+}
--- front-end/patient/script.js
+++ front-end/patient/script.js
@@ -1,3 +1,12 @@
+$.ajax({
+  method: "GET",
+  url: "https://reqres.in/api/users",
+  success: function (response) {
+    myArray = response.data;
+    buildTable(myArray);
+    console.log(myArray);
+  },
+});
 var upButton = document.getElementById("upButton");
 var downButton = document.getElementById("downButton");
 var circle = document.getElementById("circle");
@@ -16,3 +25,12 @@ downButton.onclick = function () {
   circle.style.transform = sum;
   rotate = sum;
 };
+
+function buildTable(data) {
+  var table = document.getElementById("lol");
+
+  for (var i = 0; i < data.length; i++) {
+    var name = `${data[i].id}`;
+    table.innerHTML += name;
+  }
+}
